6af7b7922d076a0ac0177d741abab35210b9abd2
pota <potajuice@gmail.com>
2022-03-12 10:56:47 +0200

    Some refactors and updates with todos

--- contracts/FinalFighters.sol
+++ contracts/FinalFighters.sol
@@ -6,15 +6,15 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract NFT is ERC721Enumerable, Ownable {
+contract FinalFighters is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     string public baseURI;
     string public baseExtension = ".json";
-    uint256 public cost = 0.05 ether;
-    uint256 public maxSupply = 10000;
-    uint256 public maxMintAmount = 7;
-    uint256 public nftPerAddressLimit = 7;
+    uint public cost = 0.05 ether;
+    uint constant maxSupply = 10000;
+    uint public maxMintAmount = 7;
+    uint public nftPerAddressLimit = 7;
     bool public paused = false;
     bool public onlyWhitelisted = true;
     address[] public whitelistedAddresses;
@@ -25,23 +25,23 @@ contract NFT is ERC721Enumerable, Ownable {
     }
 
     // public
-    function mint(uint256 _mintAmount) public payable {
+    function mint(uint mintAmount) public payable {
         require(!paused, "the contract is paused");
-        uint256 supply = totalSupply();
-        require(_mintAmount > 0, "need to mint at least 1 NFT");
-        require(_mintAmount <= maxMintAmount, "max mint amount per session exceeded");
-        require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+        require(mintAmount > 0, "need to mint at least 1 NFT");
+        require(mintAmount <= maxMintAmount, "max mint amount per session exceeded");
+        require(totalSupply() + mintAmount <= maxSupply, "max NFT limit exceeded");
 
         if (msg.sender != owner()) {
             if(onlyWhitelisted) {
                 require(isWhitelisted(msg.sender), "user is not whitelisted");
                 uint256 ownerMintedCount = addressMintedBalance[msg.sender];
+                //TODO: Should limit only apply to whitelisted or no?? maybe to everyone except admin ??
                 require(ownerMintedCount + _mintAmount <= nftPerAddressLimit, "max NFT per address exceeded");
             }
-            require(msg.value >= cost * _mintAmount, "insufficient funds");
+            require(msg.value >= cost * mintAmount, "insufficient funds");
         }
 
-        for (uint256 i = 1; i <= _mintAmount; i++) {
+        for (uint i = 1; i <= mintAmount; i++) {
             addressMintedBalance[msg.sender]++;
             _safeMint(msg.sender, supply + i);
         }
@@ -56,10 +56,11 @@ contract NFT is ERC721Enumerable, Ownable {
         return false;
     }
 
+    //TODO: Is this method really required ? Seems it has alternatives inherited from ERC721
     function walletOfOwner(address _owner) public view returns (uint256[] memory) {
-        uint256 ownerTokenCount = balanceOf(_owner);
-        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-        for (uint256 i; i < ownerTokenCount; i++) {
+        uint ownerTokenCount = balanceOf(_owner);
+        uint[] memory tokenIds = new uint[](ownerTokenCount);
+        for (uint i = 0; i < ownerTokenCount; i++) {
             tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
         }
         return tokenIds;
@@ -71,12 +72,10 @@ contract NFT is ERC721Enumerable, Ownable {
             "ERC721Metadata: URI query for nonexistent token"
         );
 
-        string memory currentBaseURI = _baseURI();
-        return bytes(currentBaseURI).length > 0 ?
-            string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)) :
+        return bytes(_baseURI()).length > 0 ?
+            string(abi.encodePacked(_baseURI(), tokenId.toString(), baseExtension)) :
             "";
     }
-
 
     function setNftPerAddressLimit(uint256 _limit) public onlyOwner {
         nftPerAddressLimit = _limit;
@@ -111,20 +110,13 @@ contract NFT is ERC721Enumerable, Ownable {
         whitelistedAddresses = _users;
     }
 
-    function withdraw() public payable onlyOwner {
-        // This will payout the owner 100% of the contract balance.
-        // Do not remove this otherwise you will not be able to withdraw the funds.
-        // =============================================================================
+    function withdrawAll() public payable onlyOwner {
         (bool os, ) = payable(owner()).call{value: address(this).balance}("");
         require(os);
-        // =============================================================================
     }
 
-    function withdraw2() public payable onlyOwner {
-        // This will payout the owner 25% of the contract balance.
-        // Do not remove this otherwise you will not be able to withdraw the funds.
-        // =============================================================================
-        (bool os, ) = payable(owner()).call{value: address(this).balance * 25 / 100}("");
+    function withdraw(uint8 percentage) public payable onlyOwner {
+        (bool os, ) = payable(owner()).call{value: address(this).balance * percentage / 100}("");
         require(os);
     }
 
